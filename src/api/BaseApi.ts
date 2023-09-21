2cf9e9fa70e72fafcdeaae3c4d8ee82a3e08f891
SirMaik <mianorsi@ciencias.unam.mx>
2023-09-20 20:10:33 -0600

    feat(api): Base API Created a base API set of functions to make http calls

---
+++ src/api/BaseApi.ts
@@ -0,0 +1,56 @@
+export type Headers = Record<string, string>;
+export type Params = Record<string, string>;
+
+async function http<T>(
+  path: string,
+  params?: Params,
+  init?: RequestInit
+): Promise<T> {
+  path = appendParams(path, params);
+
+  const request = new Request(path, init);
+  const response = await fetch(request);
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+function appendParams(path: string, params?: Params): string {
+  if (!params) {
+    return path;
+  }
+
+  return `${path}?${new URLSearchParams(params).toString()}`;
+}
+
+export async function get<T>(
+  path: string,
+  params?: Params,
+  headers?: Headers
+): Promise<T> {
+  const init = { method: "GET", headers };
+  return await http<T>(path, params, init);
+}
+
+export async function post<T, U>(
+  path: string,
+  body: U,
+  params?: Params,
+  headers?: Headers
+): Promise<T> {
+  const init = { method: "POST", headers, body: JSON.stringify(body) };
+  return await http<T>(path, params, init);
+}
+
+export async function patch<T, U>(
+  path: string,
+  body: U,
+  params?: Params,
+  headers?: Headers
+): Promise<T> {
+  const init = { method: "PATCH", headers, body: JSON.stringify(body) };
+  return await http<T>(path, params, init);
+}
