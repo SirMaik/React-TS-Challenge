0f45c5a3a7a3067a13fa1a8c9de244baf96cf1e3
SirMaik <mianorsi@ciencias.unam.mx>
2023-09-22 19:18:55 -0600

    chore(feat): Added and configured mantine

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import "./global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Movie from "./pages/Movie";
+import { MantineProvider, createTheme } from "@mantine/core";
 
 /**
  * The starting page for your App
@@ -14,19 +15,21 @@ const queryClient = new QueryClient({});
 class App extends React.Component {
   render(): JSX.Element {
     return (
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Header />
-          <main>
-            <section>
-              <Routes>
-                <Route path={"/"} element={<Home />} />
-                <Route path={"/movie/:id"} element={<Movie />} />
-              </Routes>
-            </section>
-          </main>
-        </BrowserRouter>
-      </QueryClientProvider>
+      <MantineProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Header />
+            <main>
+              <section>
+                <Routes>
+                  <Route path={"/"} element={<Home />} />
+                  <Route path={"/movie/:id"} element={<Movie />} />
+                </Routes>
+              </section>
+            </main>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </MantineProvider>
     );
   }
 }
