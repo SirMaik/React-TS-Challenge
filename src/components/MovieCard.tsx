feb78bee3ad94147f9c1e5c2edc049eca576b3b1
SirMaik <mianorsi@ciencias.unam.mx>
2023-09-22 22:18:34 -0600

    feat(mantine): Adjusted movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Movie as MovieProp } from "../api/Movie/interface";
 import posterNotFound from "../assets/posterNotFound.png";
 import { Link } from "react-router-dom";
-import { Card, Image, Text } from "@mantine/core";
+import { Card, Image, Text, Container } from "@mantine/core";
 
 /**
  * This should be a React component that, at the very least, comprises an image component a title and a description or subheading.
@@ -20,10 +20,11 @@ const MovieCard = (props: MovieProp): JSX.Element => {
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Card.Section>
           <Image
-            src={posterPath ?? posterNotFound}
+            src={posterPath}
             radius="md"
-            w="90%"
-            alt={title}
+            fallbackSrc={posterNotFound}
+            mah="250px"
+            mih="250px"
           />
         </Card.Section>
 
